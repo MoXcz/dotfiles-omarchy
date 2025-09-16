96f92c0f606a8e2f8be45b7467825e6357f39474
MoXcz <130593875+MoXcz@users.noreply.github.com>
2025-09-15 22:46:41 -0600

    feat: better tmux-sessionizer fuzzy finder

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-# The dollar sign indicates that whatever is the output that is inside of it,
-# it will be stored and then saved in session.
-# session=$(find ~ ~/indv/ -mindepth 1 -maxdepth 1 -type d | fzf)
-
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/ ~/Documents ~/workspace/ ~/workspace/github.com/ ~/workspace/codecrafter.io/ -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/ ~/Documents ~/workspace/ -mindepth 1 -maxdepth 2 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
