f3b44c1a4774f4957a1c3f056aaf8de9341750fd
MoXcz <130593875+MoXcz@users.noreply.github.com>
2025-12-15 20:21:03 -0600

    feat: install script
    still very clunky

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+DIR=$(pwd)
+REPO_URL="https://github.com/MoXcz/dotfiles-omarchy"
+REPO_NAME="dotfiles"
+
+if ! pacman -Qi "stow" &> /dev/null; then
+  echo -e "Install stow:\n  pacman -S stow"
+  exit 1
+fi
+
+cd $HOME
+
+if [ -d "$REPO_NAME" ]; then
+  echo "Repository '$REPO_NAME' already exists. Skipping clone"
+else
+  git clone "$REPO_URL" "$REPO_NAME"
+fi
+
+if [ $? -eq 0 ]; then
+  echo "Removing omarchy dotfiles 'config' files..."
+  rm -rf ~/.config/nvim \
+    ~/.config/starship.toml \
+    ~/.local/share/nvim/ \
+    ~/.local/share/nvim/ \
+    ~/.cache/nvim/ \
+    ~/.config/hypr/
+
+  cd "$REPO_NAME"
+  stow .
+else
+  echo "Failed to clone repository"
+  exit 1
+fi
+
+echo "Installing packages..."
+
+yay -S \
+  zsh zen-browser syncting \
+  tmux syncthing protonplus ghostty \
+  vulkan-radeon vulkan-tools \
+  ttf-iosevka-term ttf-iosevkaterm-nerd
